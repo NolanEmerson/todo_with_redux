bbe8dedcb4208d7de4122e42884894645b43e5cf
Nolan <nemerson17@gmail.com>
2018-04-09 13:07:48 -0700

    Created middleware to handle promises

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './reducers';
-import promise from 'redux-promise';
 import {
     BrowserRouter as Router
 } from 'react-router-dom';
+import logger from './middleware/logger';
+import promise from './middleware/promise';
 
 import App from './components/app';
 
-const store = createStore(rootReducer, {}, applyMiddleware(promise));
+const store = createStore(rootReducer, {}, applyMiddleware(logger, promise));
 
 ReactDOM.render(
     <Provider store={store}>
